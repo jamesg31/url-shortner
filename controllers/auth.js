6dbb349187c0bb1832724c2c8ce28c305a2ddeff
James Gardner <jamie.gardner25@gmail.com>
2021-10-17 15:06:21 -0700

    Updated Frontend Build

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const { OAuth2Client } = require('google-auth-library');
 const db = require('../database');
-const clientId = '1064340871794-5mn674kkvp5sdjnb7bdjoukrr77ao5pq.apps.googleusercontent.com';
+const clientId = '840964669560-lv3dkc9uuc15d24fr3e1tk46tq29k2fa.apps.googleusercontent.com';
 const client = new OAuth2Client(clientId);
 
 /* authenticate google login, send back session
